b29e03c255abfe44eaa189a6a4d2df5dd059ba9c
Bruno Candia <bruno.candia@kineticadvantage.com>
2025-01-03 15:37:56 -0300

    Minor changes done

--- PieShop.Database.Initialization/Worker.cs
+++ PieShop.Database.Initialization/Worker.cs
@@ -102,7 +102,11 @@ public class Worker : BackgroundService
         {
             if (!await dbCreator.ExistsAsync())
             {
+                Console.WriteLine("Creating Database");
+
                 await dbCreator.CreateAsync();
+
+                Console.WriteLine("Database Created");
             }
         });
     }
@@ -122,7 +126,7 @@ public class Worker : BackgroundService
                 await pieShopContext.Database.MigrateAsync();
                 ////await transaction.CommitAsync();
 
-                Console.WriteLine("Applied migrations");
+                Console.WriteLine("Migrations Applied");
             }
         });
     }
@@ -149,7 +153,7 @@ public class Worker : BackgroundService
         {
             await pieShopContext.Category.AddRangeAsync(Categories.Select(c => c.Value));
 
-            Console.WriteLine("New data...");
+            Console.WriteLine("New Category data...");
         }
 
         if (!await pieShopContext.Pie.AnyAsync())
@@ -174,9 +178,9 @@ public class Worker : BackgroundService
                 new Pie { Name = "Strawberry Cheese Cake", Price = 18.95M, ShortDescription = "You'll love it!", LongDescription = "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.", Category = Categories["Cheese cakes"], ImageUrl = "https://stpieshop.blob.core.windows.net/files/pieshop/cheesecakes/strawberrycheesecake.jpg", IsInStock = false, IsPieOfTheWeek = false, ImageThumbnailUrl = "https://stpieshop.blob.core.windows.net/files/pieshop/cheesecakes/strawberrycheesecakesmall.jpg", AllergyInformation = "" }
             );
 
-            Console.WriteLine("New data...");
+            Console.WriteLine("New Pie data...");
         }
 
-        Console.WriteLine("Seeded data...");
+        Console.WriteLine("Data Seeded");
     }
 }
